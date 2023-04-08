2d9b7678d57ec07485417bc91dc7c5b80f3ea0e4
lakindu hewawasam <lakindu2002@live.com>
2023-04-08 10:03:59 +0530

    updated the proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://axdcc2rf11.execute-api.ap-southeast-1.amazonaws.com',
+      target: 'https://a9tf5wkei0.execute-api.ap-southeast-1.amazonaws.com',
       pathRewrite: {
         '^/api': ''
       },
